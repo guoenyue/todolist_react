a4882e07d89e011ab0b91b60d1aaf38f8e780049
guoenyue <guo81482489@126.com>
2018-03-27 13:06:30 +0800

    添加htmlwebpackplugin插件来管理html模版

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-console.log(__dirname,"配置绝对地址是：")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const config={
-    mode: 'production',
+    mode: 'development',
     entry:{
         index:"./js/index.js"
     },
     output:{
         filename:"[name].bundle.js",
-        //path:path.resolve(__dirname,"dist/js")
-        path:__dirname+"/dist/js"
+        path:path.resolve(__dirname,"dist/js")
+        //path:path.resolve(__dirname,"dist")
     },
     module:{
         rules:[
@@ -25,19 +25,7 @@ const config={
             },
             {
                 test:/\.s?css$/i,
-                //loader:ExtractTextPlugin.extract("style-loader", "css-loader")
                 use:ExtractTextPlugin.extract("css-loader")
-                // [
-                //     // {
-                //     //     loader:"style-loader"
-                //     // },
-                //     // {
-                //     //     loader:"css-loader"
-                //     // }
-                //     {
-                //         loader:ExtractTextPlugin.extract("style-loader", "css-loader")
-                //     }
-                // ]
             }
         ]
     },
@@ -45,8 +33,18 @@ const config={
         extensions: [".js", ".json",".jsx",".css"]
     },
     plugins:[
-        new ExtractTextPlugin("../css/[name].css")//则会生成一个css文件
-    ]
+        new ExtractTextPlugin("../css/[name].css"),//则会生成一个css文件
+        new HtmlWebpackPlugin({
+            title:'app',
+            template: './index.html',
+            filename:"../index.html",
+            inject: 'body',
+            chunks:true
+        })
+    ],
+    devServer:{
+        contentBase:path.join(__dirname,"dist")
+    }
 };
 //特别注意此处，处理css时候，一定要将style-loader写在前边，css-loader写在后边处理，否则会报错
 module.exports=config;
